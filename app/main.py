fa29e359141a5371e8b406d7bcd445cce7d89825
Marcella Durães <mahduraes1250@gmail.com>
2022-10-21 09:45:29 -0300

    v2

--- app/main.py
+++ app/main.py
@@ -1,53 +1,49 @@
 from flask import Flask, request
 app = Flask(__name__)
+produtos = [
+    {
+       "id": 1,
+       "produto": "Sapato",
+       "preco": "R$:99,99"
+    },
+    {
+       "id": 2,
+       "produto": "Bolsa",
+       "preco": "R$:103,89"
+    },
+    {
+       "id": 3,
+       "produto": "Camisa",
+       "preco": "R$:49,98"
+    },
+    {
+       "id": 4,
+       "produto": "Calça",
+       "preco": "R$:89,72"
+    },
+    {
+       "id": 5,
+       "produto": "Blusa",
+       "preco": "R$:97,35"
+    }]
 # http://127.0.0.1:5000/teste/1
-@app.route('/teste/1', methods=['POST'])
-
-# {
-#   "estoque": [
-#    {
-#       "id": 1
-#       "produto": "Sapato"
-#       "preco": "R$:99,99"
-#    },
-#    {
-#       "id": 2
-#       "produto": "Bolsa"
-#       "preco": "R$:103,89"
-#    },
-#    {
-#       "id": 3
-#       "produto": "Camisa"
-#       "preco": "R$:49,98"
-#    },
-#    {
-#       "id": 4
-#       "produto": "Calça"
-#       "preco": "R$:89,72"
-#    },
-#    {
-#       "id": 5
-#       "produto": "Blusa"
-#       "preco": "R$:97,35"
-#    }
-# ]
-# }
-
+@app.route('/teste/1', methods=['GET'])
 def teste_json():
+    id = -1
     objeto_json = request.get_json()
     if objeto_json is not None:
         if 'id' in objeto_json:
             id = objeto_json['id']
-        if 'produto' in objeto_json:
-            produto = objeto_json['produto']
-        if 'preco' in objeto_json:
-            preco = objeto_json['preco']
+    resp = {'produto': '', 'preco': None} 
+    for produto in produtos: 
+        if produto['id'] == id: 
+            resp = produto
 
-        return '''
+    return '''
             ID: {}
             Produto: {}
             Preco: {}
-            '''.format(id,produto,preco)
+            '''.format(resp['id'],resp['produto'],resp['preco'])
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)
